969c28e141511d216b22c9e586d5fe9a8463713d
Wes Payne <noblepayne@noblepayne.com>
2023-04-28 12:18:55 -0700

    Add encrypt/decrypt scripts

---
+++ decrypt.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+age --decrypt -i age_key.txt -o ./decrypted_secrets ./secrets
---
+++ encrypt.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+<decrypted_secrets age -r age1x96vxjw8kk855k6tf50dvrw0usd2f0nd09nwl5juv96c8jd76p8su3xgrw -o secrets
