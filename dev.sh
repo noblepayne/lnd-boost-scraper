e2eb7fda1b2795c43dbab223100815a1a08af219
Wes Payne <noblepayne@noblepayne.com>
2024-08-06 13:14:46 -0700

    cleanup v1 legacy

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-bb -cp $(clj -Spath -Sdeps '{:deps {djblue/portal {:mvn/version "0.38.1"}}}') nrepl-server
+clj -Sdeps '{:deps {datalevin/datalevin {:mvn/version "0.8.25"} nrepl/nrepl {:mvn/version "1.1.0"} org.babashka/http-client {:mvn/version "0.3.11"} org.clojure/core.async {:mvn/version "1.6.681"}}}' -J--add-opens -J"java.base/java.nio=ALL-UNNAMED" -J--add-opens -J"java.base/sun.nio.ch=ALL-UNNAMED" -J-D"jdk.internal.httpclient.disableHostnameVerification" -M -m nrepl.cmdline
--- dev2.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-clj -Sdeps '{:deps {datalevin/datalevin {:mvn/version "0.8.25"} nrepl/nrepl {:mvn/version "1.1.0"} org.babashka/http-client {:mvn/version "0.3.11"} org.clojure/core.async {:mvn/version "1.6.681"}}}' -J--add-opens -J"java.base/java.nio=ALL-UNNAMED" -J--add-opens -J"java.base/sun.nio.ch=ALL-UNNAMED" -J-D"jdk.internal.httpclient.disableHostnameVerification" -M -m nrepl.cmdline
