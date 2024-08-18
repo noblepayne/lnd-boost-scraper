f955c4e6746a681cc7c8d0b3dae3a28583341512
Wes Payne <noblepayne@noblepayne.com>
2024-08-18 02:07:07 -0700

    plumbing for autoscrape

--- src/boost_scraper/upstreams/alby.clj
+++ src/boost_scraper/upstreams/alby.clj
@@ -10,7 +10,7 @@
 (defn get-boosts [{:keys [:token :items :page :wait :after :since] :as last_}]
   (when @scrape-can-run
     (println "still going!" page)
-    (let [query-params {:items items :page page}
+    (let [query-params {:items (or items 100) :page page}
           query-params (if after
                          (assoc query-params
                                 "q[created_at_gt]"
@@ -43,20 +43,44 @@
       #_(reset! get-boosts-state (:next next_))
       next_)))
 
-(defn get-all-boosts [token items-per-page & get-boost-args]
-  (iteration get-boosts
-             {:somef (comp seq :data)
-              :kf :next
-              :initk (into {:token token
-                            :items items-per-page
-                            :page 1}
-                           (apply hash-map get-boost-args))}))
+(defn get-all-boosts [token #_items-per-page & get-boost-args]
+  (map :data
+       (iteration get-boosts
+                  {:somef (comp seq :data)
+                   :kf :next
+                   :initk (into {:token token
+                                 #_:items #_items-per-page
+                                 :page 1}
+                                (apply hash-map get-boost-args))})))
+
+(defn get-all-boosts-until-epoch [token epoch & get-boost-args]
+  (->> {:somef (comp seq :data)
+        :kf :next
+        :initk (into {:token token
+                      :page 1}
+                     (apply hash-map get-boost-args))}
+       (iteration get-boosts)
+       (map :data)
+       (take-while
+        (fn [boost-batch]
+          (let [filtered-batch (filter :creation_date boost-batch)
+                creation_dates (map :creation_date filtered-batch)
+                first_creation_date (apply min creation_dates)]
+            (<= epoch first_creation_date))))))
 
 (comment
 
-  (def test-token (or (System/getenv "ALBY_ACCESS_TOKEN") ""))
+  (def test-token (or (System/getenv "ALBY_ACCESS_CODE") ""))
 
   (get-boosts {:token test-token :items 2
                :page 1 :after #inst "2024-08-01T11:10:00"
 
-               #_:since #_"txjbH2VGZJ7Z9MzCtAaQWhJ4"}))
+               #_:since #_"txjbH2VGZJ7Z9MzCtAaQWhJ4"})
+
+  (first (get-all-boosts test-token :items 2 :wait 2000))
+  (def _
+    (into []
+          (get-all-boosts-until-epoch
+           test-token
+           (boost-scraper.core/->epoch #inst "2024-08-12T20:00")
+           :wait 3000))))
--- src/boost_scraper/upstreams/lnd.clj
+++ src/boost_scraper/upstreams/lnd.clj
@@ -19,10 +19,10 @@
 
 (defn get-lnd-boosts [{:keys [macaroon offset wait] :as last_}]
   (when @scrape-can-run
-    (println "still going!" offset)
+    (println "still going!" offset (get last_ :url))
     (let [query-params {:reversed true}
           query-params (if offset (assoc query-params :index_offset offset) query-params)
-          data (-> "https://100.115.78.27:8080/v1/invoices"
+          data (-> (get last_ :url "https://100.115.78.27:8080/v1/invoices")
                    (http/get {:headers {"Grpc-Metadata-macaroon" macaroon}
                               :client (http/client {:ssl-context {:insecure true}})
                               :query-params query-params})
@@ -51,7 +51,7 @@
       #_(reset! get-boosts-state (:next next_))
       next_)))
 
-(defn get-all-lnd-boosts [macaroon & get-boost-args]
+(defn get-all-boosts [macaroon & get-boost-args]
   (iteration get-lnd-boosts
              {:somef (comp seq :data)
               :vf :data
@@ -59,12 +59,30 @@
               :initk (into {:macaroon macaroon}
                            (apply hash-map get-boost-args))}))
 
+(defn get-all-boosts-until-epoch [macaroon epoch & get-boost-args]
+  (->> {:somef (comp seq :data)
+        :vf :data
+        :kf :next
+        :initk (into {:macaroon macaroon}
+                     (apply hash-map get-boost-args))}
+       (iteration get-lnd-boosts)
+       (take-while
+        (fn [boost-batch]
+          (let [filtered-batch (filter :creation_date boost-batch)
+                creation_dates (map (comp #(Integer/parseInt %) :creation_date) filtered-batch)
+                first_creation_date (apply min creation_dates)]
+            (<= epoch first_creation_date))))))
+
 (comment
 
   (def macaroon (read-macaroon "/home/wes/src/lnd_workdir/admin.macaroon"))
+  (def nodecan-macaroon (read-macaroon "/home/wes/src/lnd_workdir/nodecan/nodecan.macaroon"))
 
   (get-lnd-boosts {:macaroon macaroon})
-  (get-all-lnd-boosts {:macaroon macaroon :wait 2000})
+  (def _ (into [] (get-all-boosts-until-epoch
+                   macaroon
+                   (boost-scraper.core/->epoch #inst "2024-08-12T20:00")
+                   :wait 500)))
 
     ;; TODO: docs on the property we need to set for this to work
   (http/get "https://100.115.78.27:8080/v1/invoices"
