a59777bb1bea0b85d00aefbef512b21d0f5c3d97
Wes Payne <noblepayne@noblepayne.com>
2026-04-14 18:32:56 -0700

    feat: enhance scraper resilience with exponential backoff and jitter

--- src/boost_scraper/upstream/lnd.clj
+++ src/boost_scraper/upstream/lnd.clj
@@ -2,7 +2,8 @@
   (:require [babashka.http-client :as http]
             [boost-scraper.upstream :as upstream]
             [boost-scraper.utils :as utils]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [clojure.math :as math]))
 
 ;; macaroon -> hex
 (defn- get-path [pathstr]
@@ -16,6 +17,27 @@
       javax.xml.bind.DatatypeConverter/printHexBinary
       (#(.toLowerCase %))))
 
+(defn with-retries [f & {:keys [max-retries base-delay-ms] :or {max-retries 3 base-delay-ms 1000}}]
+  (loop [attempt 1]
+    (let [result (try
+                   {:ok (f)}
+                   (catch Exception e
+                     (if (< attempt max-retries)
+                       {:retry e}
+                       {:error e})))]
+      (if-let [e (:retry result)]
+        (let [;; Exponential backoff: base * 2^(attempt-1)
+              exp-delay (* base-delay-ms (long (math/pow 2 (dec attempt))))
+              ;; Full Jitter: random between 0 and exp-delay
+              jitter-delay (long (rand exp-delay))]
+          (println (format "Attempt %d failed, retrying in %dms (jittered from %dms)... Error: %s"
+                           attempt jitter-delay exp-delay (.getMessage e)))
+          (Thread/sleep jitter-delay)
+          (recur (inc attempt)))
+        (if-let [e (:error result)]
+          (throw e)
+          (:ok result))))))
+
 (defrecord Scraper []
   upstream/IBoostScrape
   (get-boosts [_ {:keys [token wait offset url] :as last_}]
@@ -23,12 +45,14 @@
       (println "still going!" offset url)
       (let [query-params {:reversed true}
             query-params (if offset (assoc query-params :index_offset offset) query-params)
-            data (-> (or url "https://100.115.78.27:8080/v1/invoices")
-                     (http/get {:headers {"Grpc-Metadata-macaroon" token}
-                                :client (http/client {:ssl-context {:insecure true}})
-                                :query-params query-params})
-                     :body
-                     (json/parse-string true))
+            data (with-retries
+                   (fn []
+                     (-> (or url "https://100.115.78.27:8080/v1/invoices")
+                         (http/get {:headers {"Grpc-Metadata-macaroon" token}
+                                    :client (http/client {:ssl-context {:insecure true}})
+                                    :query-params query-params})
+                         :body
+                         (json/parse-string true))))
             next_ {:next (into last_ {:macaroon token
                                       :offset (get data :first_index_offset)})
 
